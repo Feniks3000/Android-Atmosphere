f5c1d9508c15ff9842fda41b157c1d36b3b244cc
Trofimov <iet@kalite.ro>
2020-11-02 08:57:39 +0300

    Добавил лэйауты для экрана настроек settings.xml и экрана выбора города city_choose.xml. Добавил открытие экрана настроек с экрана погоды и возврат к экрану погоды с экрана настроек.

--- app/src/main/java/ru/geekbrains/atmosphere/MainActivity.java
+++ app/src/main/java/ru/geekbrains/atmosphere/MainActivity.java
@@ -3,27 +3,39 @@ package ru.geekbrains.atmosphere;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner spinnerCity;
-    private ImageView imagePrecipitationNow;
-    private TableRow rowTempNext4Hour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.city_weather);
+        initCityWeather();
+    }
 
+    public void openSettings(View view) {
+        setContentView(R.layout.settings);
+    }
+
+    public void saveSettings(View view) {
+        setContentView(R.layout.city_weather);
+        initCityWeather();
+    }
+
+    public void initCityWeather() {
         spinnerCity = findViewById(R.id.spinnerCity);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"Moscow", "Kemerovo", "Novosibirsk"});
         spinnerCity.setAdapter(arrayAdapter);
+    }
 
-        imagePrecipitationNow = findViewById(R.id.imagePrecipitationNow);
-        rowTempNext4Hour = findViewById(R.id.rowTempNext4Hour);
+    public void initCityChoose() {
+        spinnerCity = findViewById(R.id.spinnerCityChoose);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"Moscow", "Kemerovo", "Novosibirsk"});
+        spinnerCity.setAdapter(arrayAdapter);
     }
 }
