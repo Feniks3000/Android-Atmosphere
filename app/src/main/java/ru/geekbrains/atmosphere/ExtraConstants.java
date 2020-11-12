1c8e425bc1ba06a359b22b16d7029f1d1bd9d1a8
Trofimov <iet@kalite.ro>
2020-11-13 02:21:10 +0300

    Выделил кнопки в отдельный фрагмент. Привязал создание фрагмента кнопок к инициализации/смене города, чтобы передавать в кнопки город для поиска погоды в интернете

--- app/src/main/java/ru/geekbrains/atmosphere/ExtraConstants.java
+++ app/src/main/java/ru/geekbrains/atmosphere/ExtraConstants.java
@@ -3,4 +3,5 @@ package ru.geekbrains.atmosphere;
 public interface ExtraConstants {
     String SETTINGS = "SETTINGS";
     String CITIES = "CITIES";
+    String CITY = "CITY";
 }
--- app/src/main/java/ru/geekbrains/atmosphere/MainActivity.java
+++ app/src/main/java/ru/geekbrains/atmosphere/MainActivity.java
@@ -2,8 +2,11 @@ package ru.geekbrains.atmosphere;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements ExtraConstants {
     private static final int SETTINGS_REQUEST_CODE = 1;
     private static final boolean LOGGING = false;
 
+    private boolean landscapeOrientation;
+
     private Button buttonSettings;
     private Button buttonAboutCity;
     private Spinner spinnerCity;
@@ -32,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements ExtraConstants {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        landscapeOrientation = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
 
         Toast.makeText(getApplicationContext(), String.valueOf(MyApp.getInstance().getStorage().incCounter()), Toast.LENGTH_SHORT).show();
 
@@ -40,7 +46,30 @@ public class MainActivity extends AppCompatActivity implements ExtraConstants {
 
         spinnerCity = findViewById(R.id.spinnerCity);
         initCityWeather(cities);
+        spinnerCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                CityWeatherButtonsFragment buttonsFragment = CityWeatherButtonsFragment.create(spinnerCity.getSelectedItem().toString());
+                if (landscapeOrientation) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.infoFragment, buttonsFragment).commit();
+                } else {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.buttonsFragment, buttonsFragment).commit();
+                }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        if (LOGGING) {
+            Log.i(CLASS, "onCreate");
+            Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initButtons() {
         buttonSettings = findViewById(R.id.settingsButton);
         buttonSettings.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
@@ -55,11 +84,6 @@ public class MainActivity extends AppCompatActivity implements ExtraConstants {
             intent.putExtra(SearchManager.QUERY, String.format(getResources().getString(R.string.weatherIn), spinnerCity.getSelectedItem().toString()));
             startActivity(intent);
         });
-
-        if (LOGGING) {
-            Log.i(CLASS, "onCreate");
-            Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
-        }
     }
 
     private Cities initCities() {
