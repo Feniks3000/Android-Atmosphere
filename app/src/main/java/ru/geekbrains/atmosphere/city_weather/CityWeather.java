3a9aac93c8da5e1c45bf19f13ebe68cce4f17ba7
Trofimov <iet@kalite.ro>
2020-11-19 09:21:19 +0300

    Сделал RecyclerView с карточками city_weather_item.xml с информацией о погоде в городах

---
+++ app/src/main/java/ru/geekbrains/atmosphere/city_weather/CityWeather.java
@@ -0,0 +1,107 @@
+package ru.geekbrains.atmosphere.city_weather;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.List;
+
+public class CityWeather implements Parcelable {
+    private String city;
+    private Integer temperature;
+    private Integer picture;
+    private List<HourWeather> next4HoursWeather;
+    private List<DayWeather> next5DaysWeather;
+
+    public CityWeather(String city, Integer temperature, Integer picture, List<HourWeather> next4HoursWeather, List<DayWeather> next5DaysWeather) {
+        this.city = city;
+        this.temperature = temperature;
+        this.picture = picture;
+        this.next4HoursWeather = next4HoursWeather;
+        this.next5DaysWeather = next5DaysWeather;
+    }
+
+    protected CityWeather(Parcel in) {
+        city = in.readString();
+        temperature = in.readInt();
+        picture = in.readInt();
+        next4HoursWeather = in.createTypedArrayList(HourWeather.CREATOR);
+        next5DaysWeather = in.createTypedArrayList(DayWeather.CREATOR);
+    }
+
+    public static final Creator<CityWeather> CREATOR = new Creator<CityWeather>() {
+        @Override
+        public CityWeather createFromParcel(Parcel in) {
+            return new CityWeather(in);
+        }
+
+        @Override
+        public CityWeather[] newArray(int size) {
+            return new CityWeather[size];
+        }
+    };
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Integer temperature) {
+        this.temperature = temperature;
+    }
+
+    public Integer getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Integer picture) {
+        this.picture = picture;
+    }
+
+    public List<HourWeather> getNext4HoursWeather() {
+        return next4HoursWeather;
+    }
+
+    public void setNext4HoursWeather(List<HourWeather> next4HoursWeather) {
+        this.next4HoursWeather = next4HoursWeather;
+    }
+
+    public List<DayWeather> getNext5DaysWeather() {
+        return next5DaysWeather;
+    }
+
+    public void setNext5DaysWeather(List<DayWeather> next5DaysWeather) {
+        this.next5DaysWeather = next5DaysWeather;
+    }
+
+    @Override
+    public String toString() {
+        return "CityWeather{" +
+                "city='" + city + '\'' +
+                ", temperature=" + temperature +
+                ", picture=" + picture +
+                ", next4HoursWeather=" + next4HoursWeather +
+                ", next5DaysWeather=" + next5DaysWeather +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(city);
+        dest.writeInt(temperature);
+        dest.writeInt(picture);
+        dest.writeTypedList(next4HoursWeather);
+        dest.writeTypedList(next5DaysWeather);
+    }
+}
