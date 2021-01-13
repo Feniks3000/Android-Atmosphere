1e1cabd6fd9d02dddf688841cdc7a9a185dda83c
Trofimov <iet@kalite.ro>
2021-01-13 09:25:54 +0300

    Добавил TODO на будущее

--- app/src/main/java/ru/geekbrains/atmosphere/data_request/DataRequestService.java
+++ app/src/main/java/ru/geekbrains/atmosphere/data_request/DataRequestService.java
@@ -45,10 +45,13 @@ public class DataRequestService extends IntentService implements ExtraConstants 
                 Log.i("DataRequestService", "start Thread");
                 for (int i = 0; i < cities.length; i++) {
                     try {
+
+                        // TODO: рассмотреть возможность отказа от службы в пользу возможностей Retrofit
                         Response<WeatherRequest> response = MyApp.getOpenWeatherApi().loadWeather(cities[i], BuildConfig.WEATHER_API_KEY).execute();
                         if (response.body() != null) {
                             WeatherRequest weatherRequest = (WeatherRequest) response.body();
 
+                            // TODO: найти погодный сервис с бесплатным возвратом погоды по часам и дням
                             List<HourWeather> next4Hours = new ArrayList<>();
                             for (int j = 0; j < 4; j++) {
                                 next4Hours.add(new HourWeather(String.valueOf(10 + j), j * 10 + random.nextInt(10)));
@@ -61,8 +64,14 @@ public class DataRequestService extends IntentService implements ExtraConstants 
                                 next5Days.add(new DayWeather(String.valueOf(10 + j), morningTemperature, middayTemperature, eveningTemperature));
                             }
 
-                            // TODO: доработать отображение картинок и получение температуры сразу в цельсиях
-                            CityWeather cityWeather = new CityWeather(weatherRequest.getName(), (int) (weatherRequest.getMain().getTemp() - 273.15), 0 /*images[random.nextInt(3)]*/, next4Hours, next5Days);
+                            // TODO: доработать отображение картинок и получение температуры сразу в цельсиях (может сделать выбор единиц измерения в настройках)
+                            CityWeather cityWeather = new CityWeather(
+                                    weatherRequest.getName(),
+                                    (int) (weatherRequest.getMain().getTemp() - 273.15),
+                                    0 /*images[random.nextInt(3)]*/,
+                                    next4Hours,
+                                    next5Days
+                            );
                             Log.i("DataRequestService", "get CityWeather " + cityWeather);
                             data.add(cityWeather);
                         }
